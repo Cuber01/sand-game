f11d15187c893ebddbeedae314206bafe9e195b2
Cuber01 <johnmallard631@gmail.com>
2021-08-30 10:26:41 +0200

    quit works

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,75 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
 
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
 SDL_Event event;
 
 bool running = true;
 
-
-
-int main(int argc, char* args[])
+void init()
 {
-
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
-
-
-	if( SDL_Init(SDL_INIT_VIDEO) < 0 )
+    if( SDL_Init(SDL_INIT_VIDEO) < 0 )
 	{
 		printf( "SDL failed to initialize. Error: %s\n", SDL_GetError() );
         exit(1);
     }
 
-	window = SDL_CreateWindow( "Sand game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+	window = SDL_CreateWindow("Sand game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
 	if( window == NULL )
 	{
 		printf( "Window could not be created. SDL_Error: %s\n", SDL_GetError() );
         exit(1);
 	} 
+}
+
+void quit()
+{
+	SDL_DestroyWindow( window );
+	SDL_Quit();
+}
+
+void handleEvent(SDL_Event* event)
+{
+    switch (event->type)
+    {
+    case SDL_QUIT:
+        running = false;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+int main(int argc, char* args[])
+{
+
+    init();
+
     
 	while(running)
     {
+        while( SDL_PollEvent(&event) )
+        {
+            handleEvent(&event);
+        }
+
 	    screenSurface = SDL_GetWindowSurface( window );
 	    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 		
 
 	    SDL_UpdateWindowSurface( window );
 
-        if(event.type == SDL_QUIT)
-        {
-            running = false;
-        }
+
 	    
     }
     
-
-
-	SDL_DestroyWindow( window );
-	SDL_Quit();
+    quit();
 
 	return 0;
 }
