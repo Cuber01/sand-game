050f2e697be408f2949a814566b72a847edd1dda
Cuber01 <johnmallard631@gmail.com>
2021-09-30 20:23:06 +0200

    poorly integrated imgui

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,10 @@
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
+
+const char* glsl_version;
+SDL_GLContext gl_context;
+ImGuiIO io;
 #endif
 
 
@@ -55,34 +59,43 @@ CUtil Util;
 void init()
 {
 
+    // place cursor
     Cursor.adjustCursor(20, 20, 40, 40);
-    std:srand((unsigned)time(NULL));
+    
+    // initalize rand
+    srand((unsigned)time(NULL));
 
     #ifdef GUI
-    const char* glsl_version = "#version 130";
+
+    // initialize opengl
+    glsl_version = "#version 130";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
-    // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    
     #endif
 
-    if( SDL_Init(SDL_INIT_VIDEO) < 0 )
+    // initalize video
+    if( SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         printf( "SDL failed to initialize. Error: %s\n", SDL_GetError() );
         exit(1);
     }
 
+    // setup window
     #ifdef GUI
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_SHOWN);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
     #else 
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_SHOWN);
     #endif
 
+
+    // create window
 	window = SDL_CreateWindow("Sand game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, window_flags);
 
 	if(window == NULL)
@@ -92,29 +105,36 @@ void init()
 	} 
 
     #ifdef GUI
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    // initialize opengl context
+    gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_GL_SetSwapInterval(1);
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
+    // initalize imgui
+    IMGUI_CHECKVERSION(); 
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
 
-    // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
     #endif
     
-    RenderHandler.init();
+    // //RenderHandler.init();
 
 }
 
 void quit()
 {
+    #ifdef GUI
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    #endif
+
     SDL_DestroyRenderer( RenderHandler.renderer );
 	SDL_DestroyWindow( window );
 	SDL_Quit();
@@ -122,6 +142,10 @@ void quit()
 
 void handleEvent(SDL_Event* event)
 {
+
+    #ifdef GUI
+    ImGui_ImplSDL2_ProcessEvent(event);
+    #endif
 
     switch (event->type)
     {
@@ -250,15 +274,20 @@ void updateParticles()
 
 }
 
-
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 int main(int argc, char* args[])
 {
-
     init();
 
-    
-	while(running)
+    RenderHandler.init();
+
+    bool show_another_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (running)
     {
+
         #ifdef SLOW
             SDL_Delay(100);
         #endif
@@ -269,22 +298,45 @@ int main(int argc, char* args[])
 
         SDL_Delay(10);
 
-        while( SDL_PollEvent(&event) )
+        Cursor.adjustCursor(Cursor.x, Cursor.y, Cursor.w, Cursor.h);   
+        handleInput();
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
         {
             handleEvent(&event);
         }
 
 
-        Cursor.adjustCursor(Cursor.x, Cursor.y, Cursor.w, Cursor.h);   
-        handleInput();
-
         memset( next, 0, sizeof(next) );
 
         updateParticles();
 
-        RenderHandler.draw();
+        RenderHandler.draw(); 
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+
 
         memcpy( grid, next, sizeof(grid) );
+
         
     }
 
